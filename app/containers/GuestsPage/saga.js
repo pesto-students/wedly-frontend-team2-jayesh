6194fe95c96e43280fd5187e0a8a4d6684d905b0
Kamal Jain <kamaljain.ts@gmail.com>
2022-10-30 00:14:37 +0530

    whatsapp token updated

--- app/containers/GuestsPage/saga.js
+++ app/containers/GuestsPage/saga.js
@@ -108,7 +108,7 @@ export async function sendInvite(from, to, mobile, userId) {
     {
       headers: {
         Authorization:
-          "Bearer EAALnfn2bArEBAPEEao0VtZCCRhZA3XZCNbxCCHEF8irW67h2829BpKEJQRNwlG4RZBVDO2Ubc3cdmZCZCCdXUZCdxxCdUxjxdxmCjdbZB7bN24HylZB1gHF2QhY2iji0j15pjY6imHZARSOX9Frm1hnqj2ly3xpLraT6ThaXzj8qNt7hgYZB0jlZCTjTc7pFfZB4zFCUp7UuZAfSgzcAZDZD",
+          "Bearer EAALnfn2bArEBAAHnnpTKAYplDGoxZBhfUAIbcnmpHtHfvlXC5aAOsoN9irVXuYrAFahdRRTnL9oNpfINkCHAGHIicZBPQZCtPLxn4cfZBVaXZBWTjCUl50Pu5gJY7ea27IDCziZBXJqcGFgZCfqsIAbX9nFM9PtuJte1Xl9arMuzOxlZAKf76DNf4LZCcZAZBkpbUyw2cnJJq0ZAUQZDZD",
         "Content-Type": "application/json",
       },
     }
