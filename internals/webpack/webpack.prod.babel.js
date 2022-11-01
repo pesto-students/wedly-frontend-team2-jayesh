b04ef6ea34f31df857802b82a22068d79f985afc
shiv123-max <shivanshusid123@gmail.com>
2022-11-01 21:47:05 +0530

    added publicPath

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -21,6 +21,7 @@ module.exports = require("./webpack.base.babel")({
   output: {
     filename: "[name].[chunkhash].js",
     chunkFilename: "[name].[chunkhash].chunk.js",
+    publicPath: "/build/",
   },
 
   optimization: {
